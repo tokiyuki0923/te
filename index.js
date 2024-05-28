d73342bad205b23421cb308043a964bf41a0249c
濱田時行 <hamadatokiyuki@hamadatokiyukinoMacBook-Pro.local>
2024-05-28 14:53:15 +0900

    やばい、リポジトリ変えて作り直したらできちゃった。一つのエンドポイントから全てのポケモン画像の取得。これから解読して、今まで作業していたリポジトリの方で同じように作りたい。今回はreactを使用していないから。いったんステイ！

---
+++ index.js
@@ -0,0 +1,34 @@
+const pokedex = document.getElementById("pokedex");
+
+console.log(pokedex);
+
+const fetchPokemon = () => {
+    const promises = [];
+    for(let i = 1; i < 899; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+}
+
+Promise.all(promises).then(results => {
+    const pokemon = results.map((data) => ({
+        name: data.name,
+        id: data.id,
+        image: data.sprites.front_default,
+        type: data.types.map((type) => type.type.name).join(', ')
+    }));
+    displayPokemon(pokemon);
+});
+};
+
+const displayPokemon = (pokemon) => {
+console.log(pokemon);
+const pokemonHTMLString = pokemon.map(pokemon => `
+    <li class="card">
+        <img class="card-image" src="${pokemon.image}" /> 
+        <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+        <p class="card-subtitle">Type: ${pokemon.type}</p>
+    </li>`).join('');
+pokedex.innerHTML = pokemonHTMLString;
+};
+
+fetchPokemon();
